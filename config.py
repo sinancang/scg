917f027b7eb113395d9cc238bee65238a5a0aeda
sinancang <sinancangulan@gmail.com>
2020-08-05 14:40:00 +0300

    first comimt

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY_BLOG')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = '587'
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SUBJECT_PREFIX = '[SCG]'
+    MAIL_SENDER = 'Sinan Can Gulan <sinancangulan@gmail.com>'
+    ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    #fetch dev dbase
+
+class TestingConfig(Config):
+    TESTING = True
+    #fetch test dbase
+
+class ProductionConfig(Config):
+    #fetch production dbase
+
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+        'default': DevelopmentConfig
+}
