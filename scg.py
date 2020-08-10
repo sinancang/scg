12e86947a8e05164dde09328ce292d3c7e357c32
sinancang <sinancangulan@gmail.com>
2020-08-10 17:03:02 +0300

    add database models and init db

--- scg.py
+++ scg.py
@@ -3,9 +3,15 @@ import os
 from app import create_app, db
 from flask_migrate import Migrate
 
-app = create_app(os.getenv('SCG_CONFIG'))
+app = create_app('development')
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db)
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
